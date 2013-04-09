d1e707ef91699ffc42d7adb6fc8adb87f3fdf544
Blopap <blopap@gmail.com>
2013-04-09 20:51:20 +0200

    Commit día 9-4-2013

---
+++ Programacion/src/ProgramacionBD/AccesoBDMySql.java
@@ -0,0 +1,58 @@
+package ProgramacionBD;
+
+import java.sql.*;
+
+public class AccesoBDMySql {
+	public static void main(String args[]){
+		
+		String driver="com.mysql.jdbc.Driver";
+		String url="jdbc:mysql://localhost:3306/TallerMecanico";
+		Connection con=null;
+		
+		try{
+			Class.forName(driver);
+			
+			con=DriverManager.getConnection(url,"root","");
+			
+			Statement stmt=con.createStatement();
+			
+			String sentenciaSQL="Select * from FacturaMecanica";
+			ResultSet rs=stmt.executeQuery(sentenciaSQL);
+			
+			String conductores, marcas;
+			int importes;
+			
+			while(rs.next())
+			{
+				conductores=rs.getString("Nombre");
+				marcas=rs.getString("Marca");
+				importes=rs.getInt("Importe");
+				
+				System.out.println("Conductor: "+conductores+". Marca: "+marcas+". Importe: "+importes);
+			}
+			
+			rs.close();
+			stmt.close();
+			
+		}
+		catch(ClassNotFoundException e){
+			System.out.println("Controlador JDBC no encontrado: "+e.toString());
+		}
+		catch(SQLException e){
+			System.out.println("Excepcion capturada de SQL: "+e.toString());
+		}
+		finally{
+			try{
+				if(con!=null)
+				{
+					con.close();
+				}
+			}
+			catch(SQLException e)
+			{
+				System.out.println("No se puede cerrar la conexion: "+e.toString());
+			}
+		}
+	}
+
+}
